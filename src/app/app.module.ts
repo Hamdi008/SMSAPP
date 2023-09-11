109964251e4dde3a47051a00952e9941da64e021
Hamdi ELFEKIH <hamdi.elfekih@outlook.com>
2023-09-11 08:59:16 +0100

    Add home and sign-in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    HomeComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
